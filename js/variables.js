0f9d26250ea3a060224d1d8acb01f52baaefcd20
JairAlves007 <ajair2550@gmail.com>
2022-06-07 19:15:54 -0300

    Projeto finalizado

--- js/variables.js
+++ js/variables.js
@@ -3,7 +3,7 @@ const allLetters = document.querySelectorAll("#board .row-board .key");
 const btnEnter = document.getElementById("btn-enter");
 const btnDel = document.getElementById("btn-del");
 const wordsContainer = document.getElementById("words-container");
-const wordSelected = ["A", "M", "O", "R"];
+const wordSelected = ["M", "I", "S", "L", "E", "N", "Y"];
 
 const lettersAllowed = [
 	"A",
