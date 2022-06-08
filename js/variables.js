01af7763983732ed15b3f01e57c7f7f03c24d649
JairAlves007 <ajair2550@gmail.com>
2022-06-08 19:17:53 -0300

    Nova palavra

--- js/variables.js
+++ js/variables.js
@@ -3,7 +3,7 @@ const allLetters = document.querySelectorAll("#board .row-board .key");
 const btnEnter = document.getElementById("btn-enter");
 const btnDel = document.getElementById("btn-del");
 const wordsContainer = document.getElementById("words-container");
-const wordSelected = ["M", "I", "S", "L", "E", "N", "Y"];
+const wordSelected = ["I", "N", "F", "I", "N", "I", "T", "O"];
 
 const lettersAllowed = [
 	"A",
