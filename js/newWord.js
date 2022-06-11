59c0985bbb2d429f1d6cec8335518b89cbaefc7a
JairAlves007 <ajair2550@gmail.com>
2022-06-11 10:08:23 -0300

    Criando sistema de criação de palavras e chances

--- js/newWord.js
+++ js/newWord.js
@@ -3,7 +3,7 @@ const form = document.forms[0];
 if (form) {
 	form.onsubmit = e => {
 		e.preventDefault();
-		let word = e.target.word.value.toUpperCase();
+		let word = String(e.target.word.value).toUpperCase();
 		let chances = +e.target.chances.value;
 
 		if (word.trim() === "" || chances < 3 || chances > 6) {
@@ -11,8 +11,7 @@ if (form) {
 			return;
 		}
 
-		localStorage.setItem("word", word);
-		localStorage.setItem("chances", chances);
+		updateData(word, chances);
 		e.target.word.value = "";
 		e.target.chances.value = "";
     
--- js/variables.js
+++ js/variables.js
@@ -3,9 +3,10 @@ const allLetters = document.querySelectorAll("#board .row-board .key");
 const btnEnter = document.getElementById("btn-enter");
 const btnDel = document.getElementById("btn-del");
 const wordsContainer = document.getElementById("words-container");
-const wordSelected = ["S", "A", "U", "D", "A", "D", "E", "S"];
+const wordSelected = [];
 
 const word = localStorage.getItem('word');
+const chances = +localStorage.getItem('chances') > 0 ? +localStorage.getItem('chances') : 6;
 
 const lettersAllowed = [
 	"A",
@@ -39,7 +40,6 @@ const lettersAllowed = [
 let rowStep;
 let letters;
 let step = 0;
-let chances = +localStorage.getItem('chances') > 0 ? +localStorage.getItem('chances') : 6;
 
 console.log(
 	"Feito com amor (e bastante estresse, programação, né KKKKKKKKKKKKKK), por Jair Alves, para Misleny Lima ❤️"
